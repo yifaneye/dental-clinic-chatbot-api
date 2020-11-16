0133d04731f6aaa0a9848fbfd07ac626e2018f52
Yifan Ai <ayfallen@hotmail.com>
2020-11-16 15:01:22 +1100

    Update error messages

--- chat/chat/v1/api/wit_ai.py
+++ chat/chat/v1/api/wit_ai.py
@@ -4,8 +4,9 @@ import requests
 from .reply import *
 from .secrets import *
 
-DEFAULT_REPLY = "Sorry, I understand your intent but I am not programmed to respond to this intent."
-ERROR_REPLY = "Sorry, I can not understand your intent from my training."
+WIT_RESPONSE_ERROR_REPLY = "Sorry, I use wit to process your intent but wit has error right now."
+INTENT_NOT_PROGRAMMED_REPLY = "Sorry, I understand your intent but I am not programmed to respond to this intent."
+WIT_CAN_NOT_MATCH_REPLY = "Sorry, I can not understand your intent from my training."
 
 
 def get_reply(message):
@@ -14,12 +15,11 @@ def get_reply(message):
     headers = {'Authorization': WIT_TOKEN}
     response = requests.get(url, headers=headers)
     if response.status_code != HTTPStatus.OK:
-        raise response.raise_for_status()
+        return WIT_RESPONSE_ERROR_REPLY
     jsonResponse = response.json()
     try:
         if jsonResponse['intents'][0]['name'] == "getDentists":
             return DentistsReply().get_reply()
-
         elif jsonResponse['intents'][0]['name'] == "getDentistInformation":
             name = jsonResponse['entities']['dentist:dentist'][0]['body']
             return DentistInformationReply(name).get_reply()
@@ -32,6 +32,6 @@ def get_reply(message):
             # Get hour 13 -> 13, 09 -> 9
             startTime = jsonResponse['entities']['wit$datetime:datetime'][0]['value'][11:13].lstrip('0') or ''
             return UpdateTimeslotReply(name, action, startTime).get_reply()
-        return DEFAULT_REPLY
+        return INTENT_NOT_PROGRAMMED_REPLY
     except:
-        return ERROR_REPLY
+        return WIT_CAN_NOT_MATCH_REPLY
