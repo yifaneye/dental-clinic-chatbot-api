ecc89aa958f25634cca0b55932544bdf687594c5
Yifan Ai <ayfallen@hotmail.com>
2020-11-16 17:04:16 +1100

    Enable CORS

--- chat/chat/__init__.py
+++ chat/chat/__init__.py
@@ -2,16 +2,19 @@
 from __future__ import absolute_import
 
 from flask import Flask
+from flask_cors import CORS
 
 import v1
 
 
 def create_app():
     app = Flask(__name__, static_folder='static')
+    CORS(app)
     app.register_blueprint(
         v1.bp,
         url_prefix='/v1')
     return app
+
 
 if __name__ == '__main__':
     create_app().run(debug=True)
